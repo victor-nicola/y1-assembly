b64f125ad8187b394ee8e0f69ac2e8a4a66d9b3a
PalamidaUsturoi <victor.t.nicola@gmail.com>
2025-09-24 14:53:33 +0200

    optimised version

--- common/branch_predictor/solution.s
+++ common/branch_predictor/solution.s
@@ -12,17 +12,8 @@ history: .quad 0
 .global init
 
 init:
-	# prologue
-	pushq %rbp
-	movq %rsp, %rbp
-
-	# save callee-saved registries
-	pushq %rbx
-	pushq %r12
-	pushq %r13
-	pushq %r14
-	pushq %r15
-	subq $8, %rsp # align stack
+	# no prologue or epilogue since we don't touch the stack
+	# not saving callee-saved since we don't touch them
 
     movq $0, %rcx
     movq $STATE_SIZE, %rdx
@@ -33,164 +24,49 @@ init:
         incq %rcx
         cmpq %rdx, %rcx
         jl init_loop
-
-	# restore callee-saved registries
-	addq $8, %rsp
-	popq %r15
-	popq %r14
-	popq %r13
-	popq %r12
-	popq %rbx
-
-	# epilogue
-	movq %rbp, %rsp
-	popq %rbp
 	ret
 
-get_index:
-	# prologue
-	pushq %rbp
-	movq %rsp, %rbp
+predict_branch:
+	# no prologue or epilogue since we don't touch the stack
+	# not saving callee-saved since we don't touch them
 
-	# save callee-saved registries
-	pushq %rbx
-	pushq %r12
-	pushq %r13
-	pushq %r14
-	pushq %r15
-	subq $8, %rsp # align stack
+	# get the index in the array for this instruction
+	# combine last bits of the address with the history
+	xorq history, %rdi # combine history and pointer
+	andq $STATE_MASK, %rdi # fit into array
+
+	movzbq state(, %rdi, 1), %rax
+
+	# get first bit of the counter
+	andq $2, %rax
+	shr $1, %rax
+	ret
+
+actual_branch:
+	# no prologue or epilogue since we don't touch the stack
+	# not saving callee-saved since we don't touch them
 
 	# get the index in the array for this instruction 
 	# combine last bits of the address with the history
 	xorq history, %rdi # combine history and pointer
 	andq $STATE_MASK, %rdi # fit into array
-	movq %rdi, %rax
-
-	# restore callee-saved registries
-	addq $8, %rsp
-	popq %r15
-	popq %r14
-	popq %r13
-	popq %r12
-	popq %rbx
-
-	# epilogue
-	movq %rbp, %rsp
-	popq %rbp
-	ret
-
-predict_branch:
-	# prologue
-	pushq %rbp
-	movq %rsp, %rbp
-
-	# save callee-saved registries
-	pushq %rbx
-	pushq %r12
-	pushq %r13
-	pushq %r14
-	pushq %r15
-	subq $8, %rsp # align stack
-
-	# get the index in the array for this instruction
-	call get_index
-
-	movzbq state(, %rax, 1), %rdx
-
-	# get first bit of the counter
-	andq $2, %rdx
-	shr $1, %rdx
-	movq %rdx, %rax
-
-	# restore callee-saved registries
-	addq $8, %rsp
-	popq %r15
-	popq %r14
-	popq %r13
-	popq %r12
-	popq %rbx
-
-	# epilogue
-	movq %rbp, %rsp
-	popq %rbp
-	ret
-
-actual_branch:
-	# prologue
-	pushq %rbp
-	movq %rsp, %rbp
-
-	# save callee-saved registries
-	pushq %rbx
-	pushq %r12
-	pushq %r13
-	pushq %r14
-	pushq %r15
-	subq $8, %rsp # align stack
-
-	# get the index in the array for this instruction 
-	call get_index
-	movq %rax, %rbx
-
-	movzbq state(, %rbx, 1), %rdx
 
 	cmpq $1, %rsi
 	je taken
 	jmp not_taken
 
 	taken:
-		# fancy math to avoid comparisons and jumps
-		# get first bit of state
-		movq %rdx, %rax
-		andq $2, %rax
-		shr $1, %rax
-
-		# get second bit of state
-		movq %rdx, %rcx
-		andq $1, %rcx
-
-		# this leaves the value to add in rax (0 or 1)
-		andq %rcx, %rax
-		not %rax
-		andq $1, %rax
-
-		add %rax, %rdx
-		jmp actual_branch_end
+		cmpb $3, state(, %rdi, 1) # if it's saturated don't increment
+		jge actual_branch_end
+		incb state(, %rdi, 1)
 	not_taken:
-		# fancy math to avoid comparisons and jumps
-		# get first bit of state
-		movq %rdx, %rax
-		andq $2, %rax
-		shr $1, %rax
-
-		# get second bit of state
-		movq %rdx, %rcx
-		andq $1, %rcx
-
-		# this leaves the value to subtract from rax (0 or 1)
-		or %rcx, %rax
-		andq $1, %rax
-
-		sub %rax, %rdx
-		jmp actual_branch_end
+		cmpb $0, state(, %rdi, 1) # if it's saturated don't decrement
+		jle actual_branch_end
+		decb state(, %rdi, 1)
 
 	actual_branch_end:
-		movb %dl, state(, %rbx, 1) # save new state
-
 		# update history
 		shlq $1, history # make space for the new answer
 		orq %rsi, history # add answer to history
 		andq $HISTORY_MASK, history # make sure we keep the history the right size
-
-		# restore callee-saved registries
-		addq $8, %rsp
-		popq %r15
-		popq %r14
-		popq %r13
-		popq %r12
-		popq %rbx
-
-		# epilogue
-		movq %rbp, %rsp
-		popq %rbp
 		ret
