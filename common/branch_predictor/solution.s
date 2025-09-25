466bc434b5781dcc0343cf90ee3ac12a8a1ad607
stefnaut <stefangmst@gmail.com>
2025-09-25 18:31:44 +0200

    added debug to check clock cycles for each predict and update + the init function

--- common/branch_predictor/solution.s
+++ common/branch_predictor/solution.s
@@ -5,6 +5,9 @@
 .equ HISTORY_MASK, (1 << HISTORY_SIZE) - 1
 state: .space STATE_SIZE
 history: .quad 0
+format_init: .asciz "Init clock cycles: %ld\n"
+format_predict: .asciz "Prediction clock cycles: %ld\n"
+format_actual: .asciz "Actual branch update clock cycles: %ld\n"
 
 .text
 .global predict_branch
@@ -15,6 +18,11 @@ init:
 	# no prologue or epilogue since we don't touch the stack
 	# not saving callee-saved since we don't touch them
 
+	rdtsc		#get time in eax:edx
+	shlq $32, %rdx	#shift rdx to the left 32 bits to make space for the lower 32 in rax
+	or %rax, %rdx	#combine upper and lower 32 bits of rdx:rax to get the clock cycle start
+	movq %rdx, %r15	#save number in r15
+
     movq $0, %rcx
     movq $STATE_SIZE, %rdx
 
@@ -24,11 +32,30 @@ init:
         incq %rcx
         cmpq %rdx, %rcx
         jl init_loop
+
+
+	rdtsc
+	shlq $32, %rdx
+	or %rax, %rdx
+	subq %r15, %rdx	#subtract start time from end time
+	movq %rdx, %rsi
+	leaq format_init(%rip), %rdi
+	xor %rax, %rax
+	call printf		#print total clock cycles (4-6 million for init function)
+	xor %rdi, %rdi
+
+
 	ret
 
 predict_branch:
 	# no prologue or epilogue since we don't touch the stack
 	# not saving callee-saved since we don't touch them
+
+	rdtsc		#get time in eax:edx
+	shlq $32, %rdx	#shift rdx to the left 32 bits to make space for the lower 32 in rax
+	or %rax, %rdx	#combine upper and lower 32 bits of rdx:rax to get the clock cycle start
+	movq %rdx, %r15	#save number in r15
+	xor %rax, %rax	#make rax 0 to make sure it doesn't hold anything
 
 	# get the index in the array for this instruction
 	# combine last bits of the address with the history
@@ -40,11 +67,32 @@ predict_branch:
 	# get first bit of the counter
 	andq $2, %rax
 	shr $1, %rax
+
+	pushq %rdi
+	pushq %rax
+
+	rdtsc
+	shlq $32, %rdx
+	or %rax, %rdx
+	subq %r15, %rdx	#subtract start time from end time
+	movq %rdx, %rsi
+	leaq format_predict(%rip), %rdi
+	xor %rax, %rax
+	call printf		#print total clock cycles (average 85 per prediction, so ~8500 for all of them)
+
+	popq %rax
+	popq %rdi
+
 	ret
 
 actual_branch:
 	# no prologue or epilogue since we don't touch the stack
 	# not saving callee-saved since we don't touch them
+	rdtsc		#get time in eax:edx
+	shlq $32, %rdx	#shift rdx to the left 32 bits to make space for the lower 32 in rax
+	or %rax, %rdx	#combine upper and lower 32 bits of rdx:rax to get the clock cycle start
+	movq %rdx, %r15	#save number in r15
+	xor %rax, %rax	#make rax 0 to make sure it doesn't hold anything
 
 	# get the index in the array for this instruction 
 	# combine last bits of the address with the history
@@ -70,4 +118,20 @@ actual_branch:
 		shlq $1, history # make space for the new answer
 		orq %rsi, history # add answer to history
 		andq $HISTORY_MASK, history # make sure we keep the history the right size
+
+		pushq %rdi
+		pushq %rax
+
+		rdtsc
+		shlq $32, %rdx
+		or %rax, %rdx
+		subq %r15, %rdx	#subtract start time from end time
+		movq %rdx, %rsi
+		leaq format_actual(%rip), %rdi
+		xor %rax, %rax
+		call printf		#print total clock cycles (average 95 per prediction, so ~9500 for all of them)
+
+		popq %rax
+		popq %rdi
+
 		ret
