2ff6fbd7f0f6b446667050d375574a72a28612ad
victor-nicola <victor.t.nicola@gmail.com>
2025-10-15 17:26:47 +0200

    smth

--- common/game/game_loop.s
+++ common/game/game_loop.s
@@ -2,7 +2,10 @@
 .section .note.GNU-stack,"",@progbits
 
 .section .data
-.equ SPRITE_SIZE, 100
+.equ GRID_COLS, 32
+.equ GRID_ROWS, 18
+.equ MAP_SIZE, (GRID_COLS * GRID_ROWS)
+
 sprite_x: .float 100.0
 sprite_y: .float 100.0
 speed: .float 10.0
@@ -31,33 +34,33 @@ render_scene:
     movq game_ren(%rip), %rdi
     call SDL_RenderClear
 
-    # draw sprite
-    # SDL_SetRenderDrawColor(renderer, 255, 100, 0, 255);
-    movq game_ren(%rip), %rdi
-    movl $255, %esi
-    movl $0, %edx
-    movl $0, %ecx
-    movl $255, %r8d
-    call SDL_SetRenderDrawColor
+    # # draw sprite
+    # # SDL_SetRenderDrawColor(renderer, 255, 100, 0, 255);
+    # movq game_ren(%rip), %rdi
+    # movl $255, %esi
+    # movl $0, %edx
+    # movl $0, %ecx
+    # movl $255, %r8d
+    # call SDL_SetRenderDrawColor
 
-    movss sprite_x(%rip), %xmm0
-    movss %xmm0, -16(%rbp) # x
-    movss sprite_y(%rip), %xmm0
-    movss %xmm0, -12(%rbp) # y
+    # movss sprite_x(%rip), %xmm0
+    # movss %xmm0, -16(%rbp) # x
+    # movss sprite_y(%rip), %xmm0
+    # movss %xmm0, -12(%rbp) # y
 
-    movl $SPRITE_SIZE, %eax
-    # convert integer to float
-    cvtsi2ss %eax, %xmm0
-    movss %xmm0, -8(%rbp) # w
+    # movl $SPRITE_SIZE, %eax
+    # # convert integer to float
+    # cvtsi2ss %eax, %xmm0
+    # movss %xmm0, -8(%rbp) # w
     
-    movl $SPRITE_SIZE, %eax
-    # convert integer to float
-    cvtsi2ss %eax, %xmm0
-    movss %xmm0, -4(%rbp) # h
+    # movl $SPRITE_SIZE, %eax
+    # # convert integer to float
+    # cvtsi2ss %eax, %xmm0
+    # movss %xmm0, -4(%rbp) # h
 
-    movq game_ren(%rip), %rdi
-    leaq -16(%rbp), %rsi
-    call SDL_RenderFillRect
+    # movq game_ren(%rip), %rdi
+    # leaq -16(%rbp), %rsi
+    # call SDL_RenderFillRect
 
     addq $16, %rsp
     movq $0, %rax # success
@@ -99,30 +102,19 @@ game_loop:
         cmpb $1, -104(%rbp) # if the left mouse button was pressed
         jne .render_frame
         
-        # the x value is stored with a 28 byte offset from the SDL_Event address
-        movss -100(%rbp), %xmm0
-        movss %xmm0, sprite_x(%rip) # update x
-        # the y value is stored with a 32 byte offset from the SDL_Event address
-        movss -96(%rbp), %xmm0
-        movss %xmm0, sprite_y(%rip) # update y
-        jmp .render_frame
+        # # the x value is stored with a 28 byte offset from the SDL_Event address
+        # movss -100(%rbp), %xmm0
+        # movss %xmm0, sprite_x(%rip) # update x
+        # # the y value is stored with a 32 byte offset from the SDL_Event address
+        # movss -96(%rbp), %xmm0
+        # movss %xmm0, sprite_y(%rip) # update y
+        # jmp .render_frame
 
         .test_key:
             # the key value is stored with a 28 byte offset from the SDL_Event address
             movl -100(%rbp), %eax # get key code
-            cmpl $0x40000052, %eax # if up arrow was pressed
-            je .move_sprite_up
 
-            cmpl $0x40000051, %eax # if down arrow was pressed
-            je .move_sprite_down
-
-            cmpl $0x40000050, %eax # if left arrow was pressed
-            je .move_sprite_left
-
-            cmpl $0x4000004f, %eax # if right arrow was pressed
-            je .move_sprite_right
-
-            cmpl $0x00000070, %eax # if p arrow was pressed
+            cmpl $0x0000001b, %eax # if esc key was pressed
             jne .render_frame
 
             call render_menu
@@ -132,30 +124,6 @@ game_loop:
 
             jmp .render_frame
 
-        .move_sprite_up:
-            movss sprite_y(%rip), %xmm0
-            subss speed(%rip), %xmm0
-            movss %xmm0, sprite_y(%rip)
-            jmp .render_frame
-
-        .move_sprite_down:
-            movss speed(%rip), %xmm0
-            addss sprite_y(%rip), %xmm0
-            movss %xmm0, sprite_y(%rip)
-            jmp .render_frame
-        
-        .move_sprite_left:
-            movss sprite_x(%rip), %xmm0
-            subss speed(%rip), %xmm0
-            movss %xmm0, sprite_x(%rip)
-            jmp .render_frame
-        
-        .move_sprite_right:
-            movss speed(%rip), %xmm0
-            addss sprite_x(%rip), %xmm0
-            movss %xmm0, sprite_x(%rip)
-            jmp .render_frame
-        
         .render_frame:
             # SDL_RenderPresent(renderer);
             movq game_ren(%rip), %rdi
--- common/game/lib/README.md
+++ common/game/lib/README.md
@@ -1,2 +1,4 @@
 # Folder for the libraries used in this project
-- SDL: C media library that allows us to play sounds and make graphics more easily in AT&T x86_64 assembly
+- SDL3: C media library that allows us to have graphics in AT&T x86_64 assembly
+- SDL3_ttf: C media library that allows us to show in game text in AT&T x86_64 assembly
+- SDL3_mixer: C media library that allows us to play sounds in AT&T x86_64 assembly
